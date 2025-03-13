27dc018214a6c6fec428752a5c28bc2d351c2c0c
Carsten Koester <carsten@ckoester.net>
2025-03-13 10:54:07 -0700

    Rename sections to cells; capture versions

--- neopixel-multi/neopixel-multi.ino
+++ neopixel-multi/neopixel-multi.ino
@@ -3,59 +3,54 @@
 /*
  * Compile-time configurables
  */
-#define LED_COUNT                     sizeof(PIXEL_SECTIONS)/sizeof(PIXEL_SECTIONS[0])
+#define LED_COUNT                     sizeof(PIXEL_CELLS)/sizeof(PIXEL_CELLS[0])
 
-// For each pixel, indicate which section it is in. Indicating 0 means this pixel in no section at all.
-const unsigned int PIXEL_SECTIONS[] = {
-   1,  1,  1,  1,  1,  1,  1,  1,  1,  0,        // Pixels 1-9 are in section 0
-   2,  2,  2,  2,  2,  2,  2,  2,  2,  0,        // Pixels 11-19 are in section 1
-   3,  3,  3,  3,  3,  3,  3,  3,  3,  0,        // Pixels 21-19 are in section 2
-   4,  4,  4,  4,  0,  0,  5,  5,  5,  5,        // Pixels 31-34 are in section 3. Pixels 37-40 are in section 4.
-   0,  6,  6,  6,  6,  6,  6,  6,  6,  6,        // Pixels 42-50 are in section 5
-   0,  7,  7,  7,  7,  7,  7,  7,  7,  7,        // Pixels 52-60 are in section 6
-   0,  8,  8,  8,  8,  8,  8,  8,  8,  8,        // Pixels 62-70 are in section 7
-   0,  0,  0,  0,  9,  9,  9,  9,  9,  9,        // Pixels 75-80 are in section 8
-   0, 10, 10, 10, 10, 10, 10
+// For each pixel, indicate which cell it is in. Indicating 0 means this pixel in no cell at all.
+const unsigned int PIXEL_CELLS[] = {
+   1,  1,  1,  1,  1,  1,  1,  1,  1,  0,        // Pixels 1-9 are in cell 1
+   2,  2,  2,  2,  2,  2,  2,  2,  2,  0,        // Pixels 11-19 are in cell 2
+   3,  3,  3,  3,  3,  3,  3,  3,  3,  0,        // Pixels 21-19 are in cell 3
+   4,  4,  4,  4,  0,  0,  5,  5,  5,  5,        // Pixels 31-34 are in cell 4. Pixels 37-40 are in cell 5.
+   0,  0,  6,  6,  6,  6,  6,  6,  6,  6,        // Pixels 43-51 are in cell 6
+   6,  0,  7,  7,  7,  7,  7,  7,  7,  7,        // Pixels 53-61 are in cell 7
+   7,  0,  8,  8,  8,  8,  8,  8,  8,  8,        // Pixels 63-71 are in cell 8
+   8,  0,  0,  0,  0,  9,  9,  9,  9,  9,        // Pixels 76-81 are in cell 9
+   9,  0, 10, 10, 10, 10, 10, 10                 // Pixels 83-88 are in cell 10
 };
 
 /*
  * Runtime configurables and variables
  */
-dmxGadget gadget("NeoMulti", LED_COUNT);
+dmxGadget gadget("NeoMC", LED_COUNT);
 
 struct dmxData {
   uint8_t red;
   uint8_t green;
   uint8_t blue;
 };
-unsigned int num_sections = 0;
+unsigned int num_cells = 0;
 
 void setup() {
   gadget.setup();
   gadget.config.advertise();
 
   for(unsigned int i=0; i<LED_COUNT; i++) {
-    if (PIXEL_SECTIONS[i] > num_sections) {
-      num_sections = PIXEL_SECTIONS[i];
+    if (PIXEL_CELLS[i] > num_cells) {
+      num_cells = PIXEL_CELLS[i];
     }
   }
-  Serial.printf("Have %d LEDs in %d sections\n", LED_COUNT, num_sections);
+  Serial.printf("Have %d LEDs in %d cells\n", LED_COUNT, num_cells);
 }
 
-// inline uint8_t pixelIntensity(unsigned int i, uint8_t value)
-// {
-//   return ((value * dmx_data[PIXEL_SECTIONS[i]].intensity)/255);
-// }
-
 void loop() {
-  dmxData dmx_data[num_sections];
+  dmxData dmx_data[num_cells];
 
   gadget.loop();
   gadget.receiver.getValues(gadget.dmxAddress.value(), sizeof(dmx_data), &dmx_data);
 
   for(unsigned int i=0; i<LED_COUNT; i++) {
-    if (PIXEL_SECTIONS[i] > 0) {
-      gadget.strip.setPixelColor(i, dmx_data[PIXEL_SECTIONS[i]-1].red, dmx_data[PIXEL_SECTIONS[i]-1].green, dmx_data[PIXEL_SECTIONS[i]-1].blue);
+    if (PIXEL_CELLS[i] > 0) {
+      gadget.strip.setPixelColor(i, dmx_data[PIXEL_CELLS[i]-1].red, dmx_data[PIXEL_CELLS[i]-1].green, dmx_data[PIXEL_CELLS[i]-1].blue);
     } else {
       gadget.strip.setPixelColor(i, 0, 0, 0);
     }
